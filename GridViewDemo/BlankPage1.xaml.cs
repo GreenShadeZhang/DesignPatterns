3b7ec32c47fe57a9c674419cfe37e1a0991028dd
zhangguangjian <zhangguangjian@huodongxing.com>
2019-03-07 16:20:07 +0800

    增量加载

---
+++ GridViewDemo/BlankPage1.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// https://go.microsoft.com/fwlink/?LinkId=234238 上介绍了“空白页”项模板
+
+namespace GridViewDemo
+{
+    /// <summary>
+    /// 可用于自身或导航至 Frame 内部的空白页。
+    /// </summary>
+    public sealed partial class BlankPage1 : Page
+    {
+        public BlankPage1()
+        {
+            this.InitializeComponent();
+            this.Loaded += MainPage_Loaded;
+        }
+      private  static BlankPage1 _blank;
+      void MainPage_Loaded(object sender, RoutedEventArgs e)
+        {
+            GridViewMain.ItemsSource = new ItemsToShow();
+            _blank = this;
+        }
+        public static BlankPage1 Blank { get { return _blank; } }
+        public ProgressBar MyProperty { get { return this.ProgressBar; } }
+    }
+}
---
+++ GridViewDemo/ItemsToShow.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Foundation;
+using Windows.UI.Core;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Data;
+
+namespace GridViewDemo
+{
+    public class ItemsToShow : ObservableCollection<string>, ISupportIncrementalLoading
+    {
+        public int lastItem = 1;
+
+        public bool HasMoreItems
+        {
+            get
+            {
+                if (lastItem > 500)
+                {
+                    return false;
+                }
+                else
+                {
+                    return true;
+                }
+            }
+        }
+
+        public IAsyncOperation<LoadMoreItemsResult> LoadMoreItemsAsync(uint count)
+        {
+            ProgressBar progressBar = BlankPage1.Blank.MyProperty;
+
+            CoreDispatcher coreDispatcher = Window.Current.Dispatcher;
+
+            return Task.Run(async () =>
+            {
+                await coreDispatcher.RunAsync(CoreDispatcherPriority.Normal,
+                    () =>
+                    {
+                         progressBar.IsIndeterminate = true;
+                         progressBar.Visibility = Visibility.Visible;
+                    });
+
+                List<string> items = new List<string>();
+                for (int i = 0; i < 100; i++)
+                {
+                    items.Add(String.Format("Item {0}", i.ToString()));
+                    lastItem++;
+                    //if (lastItem == 10000)
+                    //{
+                    //    break;
+                    //}
+                }
+
+                await coreDispatcher.RunAsync(CoreDispatcherPriority.Normal,
+                    () =>
+                    {
+                        foreach (string item in items)
+                        {
+                            this.Add(item);
+                        }
+                         progressBar.Visibility = Visibility.Collapsed;
+                         progressBar.IsIndeterminate = false;
+                    });
+
+                return new LoadMoreItemsResult() { Count = count };
+            }).AsAsyncOperation<LoadMoreItemsResult>();
+        }
+    }
+}
