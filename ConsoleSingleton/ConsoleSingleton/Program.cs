d5cf45a976c304a9e67bfc20e678a05119cd5839
gil <gil.zhang.dev@outlook.com>
2019-06-05 20:57:03 +0800

    单例模式相关

---
+++ ConsoleSingleton/ConsoleSingleton/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ConsoleSingleton
+{
+    class Program
+    {
+
+      static  Singleton singleton1 = null;
+        static Singleton singleton2 = null;
+        private static void Create()
+        {
+            singleton1 = Singleton.GetInstance();
+        }
+
+        static void Main(string[] args)
+        {
+            Thread thread = new Thread(new ThreadStart(Create));
+            thread.Start();
+            Thread thread2 = new Thread(new ThreadStart(Create2));
+            thread2.Start();
+            //Singleton singleton1 = Singleton.GetInstance();
+            //Singleton singleton2 = Singleton.GetInstance();
+            if (singleton1 == singleton2)
+            {
+                Console.WriteLine("相同");
+            }
+            Console.ReadKey();
+        }
+
+        private static void Create2()
+        {
+            singleton2 = Singleton.GetInstance();
+        }
+    }
+}
---
+++ ConsoleSingleton/ConsoleSingleton/Singleton.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleSingleton
+{
+    public class Singleton
+    {
+        private static Singleton instance;
+        private static readonly object syncRoot = new object();
+        private Singleton()
+        {
+
+        }
+        public static Singleton GetInstance()
+        {
+            if (instance == null)
+            {
+                lock (syncRoot)
+                {
+                    instance = new Singleton();
+                }
+            }
+            return instance;
+        }
+    }
+}
