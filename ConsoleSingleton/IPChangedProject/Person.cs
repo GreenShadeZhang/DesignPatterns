f8f8f79f96c6a947479d88427c9880360c8b6384
gil <gil.zhang.dev@outlook.com>
2019-06-09 23:35:00 +0800

    模拟属性通知

---
+++ ConsoleSingleton/IPChangedProject/Person.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IPChangedProject
+{
+  public  class Person:INotifyPropertyChanged
+    {
+        private string _xing;
+        private string _ming;
+        public string Xing { get { return _xing; } set { _xing = value;
+                OnPropertyChanged();
+                
+            } }
+
+        private void OnPropertyChanged([CallerMemberName]string propertyName = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        public string Ming { get { return _ming; } set { _ming = value;
+                OnPropertyChanged();
+            } }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+    }
+}
